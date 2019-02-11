b01a8987654181589823ab0a66c3bcacc10f76dc
Lenny Thomas <lennythomas03@gmail.com>
2019-02-10 20:55:57 -0500

    Updated RPS with suggested changes

--- lesson_2/rps_bonus.rb
+++ lesson_2/rps_bonus.rb
@@ -1,33 +1,27 @@
-OPTIONS = ["Please select one of the options: r = Rock"\
-          ", p = Paper, s = Scissors, l = Lizard, c = Spock"]
+OPTIONS = "Please select one of the options: r = Rock"\
+          ", p = Paper, s = Scissors, l = Lizard, c = Spock"
 VALID_CHOICES = %w(r p s l c)
 SCORE_NEEDED_TO_WIN_GAME = 5
-
-def calculate_score(user, computer)
-  running_scores = { "user_score" => 0, "computer_score" => 0 }
-
-  if win?(user, computer)
-    running_scores["user_score"] += 1
-  elsif win?(computer, user)
-    running_scores["computer_score"] += 1
-  end
-  running_scores
-end
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def get_choice()
+  prompt("Do you want to play again?(Y/N)")
+  answer = Kernel.gets().chomp()
+end
+
 def win?(first, second)
   choices_results = {
-    r: ["s", "l"],
-    p: ["r", "c"],
-    s: ["p", "l"],
-    l: ["c", "p"],
-    c: ["r", "s"]
+    "r" => ["s", "l"],
+    "p" => ["r", "c"],
+    "s" => ["p", "l"],
+    "l" => ["c", "p"],
+    "c" => ["r", "s"]
   }
 
-  choices_results.values_at(first.to_sym).flatten.include?(second)
+  choices_results.values_at(first).flatten.include?(second)
 end
 
 def display_result(player, computer)
@@ -43,11 +37,10 @@ end
 choice = ''
 user_score = 0
 computer_score = 0
-total_user_score = 0
-total_computer_score = 0
+
 loop do # main loop
   loop do # loop to get/validate choice from user
-    prompt("Choose one: #{OPTIONS.join(', ')}")
+    prompt("Choose one: #{OPTIONS}")
     choice = Kernel.gets().chomp()
     if VALID_CHOICES.include?(choice)
       break
@@ -57,38 +50,30 @@ loop do # main loop
   end
   computer_choice = VALID_CHOICES.sample
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-  user_score = calculate_score(choice, computer_choice)["user_score"]
-  total_user_score += user_score
-
-  computer_score = calculate_score(choice, computer_choice)["computer_score"]
-  total_computer_score += computer_score
+  
+  if win?(choice, computer_choice)
+    user_score += 1
+  elsif win?(computer_choice, choice)
+    computer_score += 1
+  end
 
   display_result(choice, computer_choice)
 
-  prompt("Your score: #{total_user_score},"\
-    " Computer score: #{total_computer_score}")
+  prompt("Your score: #{user_score},"\
+    " Computer score: #{computer_score}")
 
-  if total_user_score >= SCORE_NEEDED_TO_WIN_GAME
+  if user_score >= SCORE_NEEDED_TO_WIN_GAME
     prompt("You are the Grand Champion!!")
-    prompt("Do you want to play again?(Y/N)")
-    answer = Kernel.gets().chomp()
-    break unless answer.downcase().start_with?('y')
+    break unless get_choice.downcase().start_with?('y')
     system 'cls'
     user_score = 0
     computer_score = 0
-    total_user_score = 0
-    total_computer_score = 0
-  elsif total_computer_score >= SCORE_NEEDED_TO_WIN_GAME
+  elsif computer_score >= SCORE_NEEDED_TO_WIN_GAME
     prompt("The computer is champ, you lose")
-    prompt("Do you want to play again?(Y/N)")
-    answer = Kernel.gets().chomp()
-    break unless answer.downcase().start_with?('y')
+    break unless get_choice.downcase().start_with?('y')
     system 'cls'
     user_score = 0
     computer_score = 0
-    total_user_score = 0
-    total_computer_score = 0
   end
 end
-
 prompt("Thanks for playing!!")
